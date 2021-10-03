129fe66469de56782c26a6a5eaf9823a7308ce16
jumamdimu <jumamdimu@gmail.com>
2021-10-03 13:42:04 +0300

    Formatted the display
    Pull information from the https://www.nytimes.com/crosswords/game/mini HTML page
    and display on the screen like below
    
    > === ACROSS ===
    > 1. Washington paper, with "the"
    > 5. --> or <--
    > 6. From that time on
    > 7. Have a cigarette
    > 8. Some fishing gear
    > === DOWN ===
    > 1. Amazon service
    > 2. "... I guess never mind"
    > 3. Clothing items that are, in another sense, a synonym for "belts"
    > 4. Overly cutesy
    > 5. The "A" of Y.M.C.A.: Abbr.
    
    For more information reffer to README.md file
    This vertion tested and works on windows platform

--- README.md
+++ README.md
@@ -10,28 +10,46 @@
  
 # Built With (technologies used)
  Python
- BeautifulSoup
  lxml
+ webdriver
+ selenium
  
-
-#lxml is a Python library which allows for easy handling of XML and HTML files, and can also be used for web scrapping
+# lxml 
+ A Python library which allows for easy handling of XML and HTML files, used for web scrapping
+ installation: pip install lxml
+ refer to https://lxml.de/installation.html
 
 # BeautifulSoup a Python package for parsing HTML and XML documents. 
-# BeautifulSoup creates a parse tree for parsed page which is then used to extract data from HTML (https://www.nytimes.com/crosswords/game/mini)
-# BeautifulSoup is useful for web scraping.
+ BeautifulSoup creates a parse tree for parsed page which is then used to extract data from HTML (https://www.nytimes.com/crosswords/game/mini)
+ BeautifulSoup is useful for web scraping.
 
 # How to run the Application
- From the terminal window type : python 
-
+ From the terminal window type : python bot_jmd2.py
+ make sure you have change directory to the Application directory e.g C:\Users\dell\Desktop\bot_jmd2> python bot_jmd2.py
 
 # Configuration instructions
+ you must install webdriver package for the application to Run
+ -webdriver (details here https://pypi.org/project/webdriver-manager/)
+ -This can be done automatically using webdriver-manager
+ -pip install webdriver-manager
+
 # Installation instructions
+
 # Operating instructions
+ The Application will run for about 20 seconds and display the data
+
 # A file manifest (a list of files in the directory or archive)
+
 # Copyright and licensing information
+
 # Contact information for the distributor or author
+
 # A list of known bugs
+
 # Troubleshooting instructions
+
 # Credits and acknowledgments
+
 # A changelog (usually aimed at fellow programmers)
+
 # A news section (usually aimed at end users)
--- bot_jmd2.py
+++ bot_jmd2.py
@@ -1,13 +1,45 @@
-from bs4 import BeautifulSoup #import BeautifulSoup
+#Scraping Dynamic Websites with Beautiful Soup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-import requests #import requests
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-#scrape all the markup text using BeautifulSoup from https://www.nytimes.com/crosswords/game/mini
-source = requests.get('https://www.nytimes.com/crosswords/game/mini.html').text 
+#driver = Chrome(executable_path="\Applications\driver\chromedriver")
 
-#The App uses lxml library for easy handling of HTML data
-soup = BeautifulSoup(source, 'lxml') 
+driver.get('https://www.nytimes.com/crosswords/game/mini')
 
-#print(soup)
-#print out the scrapped html data in a formatted order using prettify() Library
-print(soup.prettify())
+#getting titles Across and Down
+#element = driver.find_element_by_class_name("ClueList-title--1-3oW")
+
+lst = [] # Declares an empty list named lst
+data = {}
+title_elements = driver.find_elements_by_class_name("ClueList-title--1-3oW")
+elements = driver.find_elements_by_class_name("Clue-li--1JoPu")
+for title_element in title_elements:
+    print("> === " + title_element.text + " ===") #print ACROSS and DOWN titles e.g "> === DOWN ==="
+    lst.append(title_element.text)    
+    i = 0 #iteration to retrieve the displayed numbers 
+    j = "" #used for rearranging the number and the corresponding string
+    li_number = 1
+    for element in elements: # each element includes a number and a string
+        arr = element.text.split() #split the both the number and string into an array of words
+        #print(arr)
+        for x in arr:
+            if ( i == 0 ):
+                j += "> " + x + ". " #assigning the initial string e.g "> 1."
+            else:
+                j += x + " "
+            i += 1
+        if ( li_number < 6 and title_element.text == "ACROSS" ):
+            print(j) #print the list iterms under ACROSS
+        elif ( li_number >= 6 and title_element.text == "DOWN" ):
+            print(j) #print the list iterms under DOWN
+            
+        j = ""
+        i = 0
+        li_number += 1            
+
+driver.quit()
+
+
+
