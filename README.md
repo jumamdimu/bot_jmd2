c28204815f1f2f1ba62541abe7859f0ee6ca6624
jumamdimu <jumamdimu@gmail.com>
2021-10-05 04:26:39 +0300

    Classes, Functions, and more Browers
    Enable the support of all Browsers, and rearrange the app for readability

--- README.md
+++ README.md
@@ -9,21 +9,21 @@
 	 Web scraping (https://www.nytimes.com/crosswords/game/mini) 
 	 The app employs algorithm to extract and process data from this website
  
- # Purpose
+# Purpose
 	The purpose of this App is to download and store the content in json format from https://www.nytimes.com/crosswords/game/mini. 
 	The goal of this bot is to scrap every webpage data on this web page about The Mini Crossword, so that this information (ACROSS and DOWN) 
 	can be retrieved when it's needed.	
 	The Application is called "web crawler" because crawling is a  term for automatically accessing a website and 
 	obtain data via a software program.
  
- # Execution
+# Execution
 	After the successfull execution of the Algorithm, a unique file will be created in the same folder as the App.
  
 # Built With (technologies used)
 	 Python
 	 lxml
-	 webdriver
 	 selenium
+	 webdriver-manager
  
 # lxml 
 	 A Python library which allows for easy handling of XML and HTML files, used for web scrapping
@@ -31,12 +31,14 @@
 	 refer to https://lxml.de/installation.html
 
 # BeautifulSoup a Python package for parsing HTML and XML documents. 
- BeautifulSoup creates a parse tree for parsed page which is then used to extract data from HTML (https://www.nytimes.com/crosswords/game/mini)
- BeautifulSoup is useful for web scraping.
+	 BeautifulSoup creates a parse tree for parsed page which is then used to extract data from HTML (https://www.nytimes.com/crosswords/game/mini)
+	 BeautifulSoup is useful for web scraping.
 
 # How to run the Application
- From the terminal window type : python bot_jmd2.py
- make sure you have change directory to the Application directory e.g C:\Users\dell\Desktop\bot_jmd2> python bot_jmd2.py
+	 From the terminal window type : python bot_jmd2.py
+	 make sure you have change directory to the Application directory e.g C:\Users\dell\Desktop\bot_jmd2> python bot_jmd2.py
+	 Using the executables, the double click and the App will start.
+	 The genarated .json files will be stored within the same folder of Application
 
 # Configuration instructions
 	 you must install webdriver package for the application to Run
@@ -45,31 +47,30 @@
 	 -pip install webdriver-manager
 
 # Installation instructions
-	install Selenium and chromedriver and 
+	Any browser is ok to run the Application (Mozila Firefox, google chrome, opera, IE, or Microsoft Edge)
+	
+	install Selenium and webdriver-manager
 	run the application from the dist folder (Two files one for Windows and one for Linux)
 	
 	install Selenium
 	-----------------------------
-	Setup Selenium and ChromeDriver
+	Setup Selenium and webdriver-manager
+	
 	Execute the following commands to install the required packages on your system. 
+	for Linux
 	sudo apt-get update
 	sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4
+	then
+	pip install selenium
 	
-	For windows : pip install selenium
+	For windows 
+	pip install selenium
 	reffer to : https://selenium-python.readthedocs.io/installation.html
 	
 	Selenium requires a driver to interface with the chosen browser. 
-
-	Example Chrome installation of the chromedriver, ubuntu python
-	-----------------------------
-	sudo apt-get install unzip
-	wget -N http://chromedriver.storage.googleapis.com/2.26/chromedriver_linux64.zip
-	unzip chromedriver_linux64.zip
-	chmod +x chromedriver
-	sudo mv -f chromedriver /usr/local/share/chromedriver
-	sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
-	sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
-	reffer to https://chromedriver.chromium.org/downloads
+	
+	install webdriver-manager
+	pip install webdriver-manager
 
 # Operating instructions
 	The Application will run for about 20 seconds and display the data
--- bot_jmd2.py
+++ bot_jmd2.py
@@ -1,82 +1,171 @@
 #Scraping Dynamic Websites with Selenium
-import tkinter as tk
+
+#import selenium
 from selenium import webdriver
+
+# Import webdriver_manager browser modules
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import IEDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.opera import OperaDriverManager
+
+# Import json module
 import json
+
+# Import time module
 import time
 
-# Import module
+#import tkinter module
+import tkinter as tk
+
+# Import everything from tkinter
 from tkinter import *
 
-# Create instance 
+# Create a window instance 
 win= tk.Tk() 
 
-# Adjust size
+# Adjust window size
 win.geometry("500x500")
 
-# Add a title
+# Add a title to a displayed window
 win.title("BOT JMD 2") 
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+class Bot:
+    def __init__(self, win, driver, browser_check):    
+        self.browser_check = browser_check
+        self.win = win
+        self.driver_url = driver
+        
+    def browser_ok(self):
+        try:
+            # Check for an error.
+            driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        except:
+            # Except clause:
+            self.browser_check = 1
 
-driver.get('https://www.nytimes.com/crosswords/game/mini')
+        if ( browser_check == 1 ):   
+            try:
+                # Check for an error.
+                driver = webdriver.Chrome(ChromeDriverManager().install())
+            except:
+                # Except clause:
+                self.browser_check = 2
+            
+        if ( browser_check == 2 ):    
+            try:
+                # Check for an error.
+                driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+            except:
+                # Except clause:
+                self.browser_check = 3
 
-data = {}
-data["ACROSS"] = {}
-data["DOWN"] = {}
+        if ( browser_check == 3 ):    
+            try:
+                # Check for an error.
+                driver = webdriver.Ie(IEDriverManager().install())
+            except:
+                # Except clause:
+                self.browser_check = 4
 
-Output = Text(win,                
-              bg = "white")
+        if ( browser_check == 4 ):    
+            try:
+                # Check for an error.
+                driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+            except:
+                # Except clause:
+                self.browser_check = 5
+         
+        if ( browser_check == 5 ): 
+            try:
+                # Check for an error.
+                driver = webdriver.Opera(executable_path=OperaDriverManager().install())
+            except:
+                # Except clause:
+                self.browser_check = 6
+            
+        return driver, self.browser_check
+                
+    def retrieve_data(self):
+        driver_url = self.driver_url.get('https://www.nytimes.com/crosswords/game/mini')            
+        data = {}
+        data["ACROSS"] = {}
+        data["DOWN"] = {}
 
-title = Label(win, text = "\nBOT - Extract Content and Data from a Website" + '\n')
-title.pack()
+        Output = Text(self.win, bg = "white") #handles all the data to be displayed 
+        
+        #Add header
+        title = Label(self.win, text = "\nBOT - Extract Content and Data from a Website" + '\n')
+        title.pack()
+        
+        #getting titles Across and Down
+        title_elements = self.driver_url.find_elements_by_class_name("ClueList-title--1-3oW")
 
-#getting titles Across and Down
-title_elements = driver.find_elements_by_class_name("ClueList-title--1-3oW")
+        #Retieving all elements from the list
+        elements = self.driver_url.find_elements_by_class_name("Clue-li--1JoPu")
+                
+        return title_elements, elements, Output, data, self.driver_url
+            
+    def display_data(self, title_elements, elements, Output, data, driver_url):
+        for title_element in title_elements:
+            Output.insert(END, "> === " + title_element.text + " ===" + '\n') #print ACROSS and DOWN titles e.g "> === DOWN ==="   
+            i = 0 #iteration to retrieve the displayed numbers 
+            items_holder = "" #used for rearranging the number and the corresponding string
+            li_number = 1
+            for element in elements: # each element includes a number and a string
+                arr = element.text.split() #split the both the number and string into an array of words
+                for x in arr:
+                    if ( i == 0 ):
+                        items_holder  += "> " + x + ". " #assigning the initial string e.g "> 1."
+                        index = x #get the json index as number
+                    else:
+                        items_holder += x + " " 
+                    i += 1
+                if ( li_number < 6 and title_element.text == "ACROSS" ):
+                    Output.insert(END, items_holder + '\n') #print the list iterms under ACROSS
+                    data["ACROSS"][index] = items_holder  #add the item to CROSS list
+                elif ( li_number >= 6 and title_element.text == "DOWN" ):
+                    Output.insert(END, items_holder + '\n') # print the list iterms under DOWN
+                    data["DOWN"][index] = items_holder  #add the item to DOWN list
+                items_holder  = "" #empty the item holder to recreate next item
+                i = 0 #start again to retrieve next number as part of item
+                li_number += 1 #increment list of items by 1 to retrieve the next item e.g "> 1. Washington paper, with "the""
+        Output.pack()
+        driver_url.quit()
+        return data
 
-#Retieving all elements from the list
-elements = driver.find_elements_by_class_name("Clue-li--1JoPu")
+browser_check = 0
+drivers = ""
 
-for title_element in title_elements:
-    Output.insert(END, "> === " + title_element.text + " ===" + '\n') #print ACROSS and DOWN titles e.g "> === DOWN ==="   
-    i = 0 #iteration to retrieve the displayed numbers 
-    items_holder = "" #used for rearranging the number and the corresponding string
-    li_number = 1
-    for element in elements: # each element includes a number and a string
-        arr = element.text.split() #split the both the number and string into an array of words
-        for x in arr:
-            if ( i == 0 ):
-                items_holder  += "> " + x + ". " #assigning the initial string e.g "> 1."
-                index = x #get the json index as number
-            else:
-                items_holder += x + " " 
-            i += 1
-        if ( li_number < 6 and title_element.text == "ACROSS" ):
-            Output.insert(END, items_holder + '\n') #print the list iterms under ACROSS
-            data["ACROSS"][index] = items_holder  #add the item to CROSS list
-        elif ( li_number >= 6 and title_element.text == "DOWN" ):
-            Output.insert(END, items_holder + '\n') # print the list iterms under DOWN
-            data["DOWN"][index] = items_holder  #add the item to DOWN list
-        items_holder  = "" #empty the item holder to recreate next item
-        i = 0 #start again to retrieve next number as part of item
-        li_number += 1 #increment list of items by 1 to retrieve the next item e.g "> 1. Washington paper, with "the""
+#initialize the process by checking the available browser
+browser_object = Bot(win, drivers, browser_check)
+
+#check if there is a preferable browser to load th website
+drivers, browser_check = browser_object.browser_ok() 
       
-#convert dictionary to json data
-r = json.dumps(data)
-
-# datetime object containing current date and time
-file_name = time.strftime("%Y%m%d%H%M%S")
-
-# now write output the json data to the file
-with open(file_name + ".json", "w") as f:
-    f.write(r)
+if ( browser_check == 6 ): #checking if no webdriver available
+    print("Browser error..") #incase of browser driver error, this message will display
+else: 
+    #initialize the object to retrieve data
+    bot_object = Bot(win, drivers, browser_check)
     
-driver.quit()
+    #retrieve all the data from the website
+    title_elements, elements, Output, data, driver_url = bot_object.retrieve_data()
+    
+    #display all data on the window
+    data = bot_object.display_data(title_elements, elements, Output, data, driver_url)
 
-def helloCallBack():
-   Output.insert(END, "Hello Python Hello World" + '\n')
+    # convert dictionary of all data to json data
+    r = json.dumps(data)
 
-Output.pack()
+    # datetime object containing current date and time
+    file_name = time.strftime("%Y%m%d%H%M%S")
 
-# Execute tkinter
+    # writing the json data to the json the file
+    with open(file_name + ".json", "w") as f:
+        f.write(r) 
+    
+# Execute tkinter 
 win.mainloop()
